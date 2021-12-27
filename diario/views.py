7609c72aba955fdb953c14ad5689d11defd64d48
Valdeilson <vautronica@gmail.com>
2021-12-27 20:57:35 -0300

    adicionando formularios de Login

--- diario/views.py
+++ diario/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from django.contrib.auth import login,logout,authenticate
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index (request):
-    return render(request, 'diario/login.html')
+    if request.POST:
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            cd=form.cleaned_data
+            user=authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request,user)
+                return HttpResponse('<h1>Olá {} </h1>' .format(user))
+            else:
+                return HttpResponse('<h1> Usuário ou senha incorreta </h1')
+    form=LoginForm()
+    context={
+        'form':form
+    }
+    return render(request, 'diario/login.html', context)
